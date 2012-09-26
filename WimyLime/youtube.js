0e259983061f50a5fb97ff2c36406d7efe61e791
zelon <zelonion@gmail.com>
2012-09-27 00:08:27 +0900

    Changed to html5 youtube

--- WimyLime/youtube.js
+++ WimyLime/youtube.js
@@ -1,4 +1,5 @@
 
+/*
 google.load("swfobject", "2.1");
 
 var ytplayer = null;
@@ -25,3 +26,45 @@ function _run()
 }
 
 google.setOnLoadCallback(_run);
+*/
+
+
+// 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('videoDiv', {
+      width: '480',
+      height: '300',
+      videoId: '3fy4cqWMhyI',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
